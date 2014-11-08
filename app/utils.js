b784256e7515775f66cc9a0a5a3ce18fb0d64586
Heather Arthur <fayearthur@gmail.com>
2014-11-07 23:36:01 -0800

    use 2-space indent in utils.js

--- app/utils.js
+++ app/utils.js
@@ -1,45 +1,47 @@
 var utils = {
-   dateAgo : function(daysAgo) {
-      daysAgo = daysAgo || 0;
-      var dayMs = 1000 * 60 * 60 * 24;
-      return new Date(Date.now() - (dayMs * daysAgo));
-   },
+  dateAgo: function(daysAgo) {
+    daysAgo = daysAgo || 0;
+    var dayMs = 1000 * 60 * 60 * 24;
+    return new Date(Date.now() - (dayMs * daysAgo));
+  },
 
-   dateString : function(daysAgo) {
-      var date = utils.dateAgo(daysAgo);
-      return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
-   },
+  dateString: function(daysAgo) {
+    var date = utils.dateAgo(daysAgo);
+    return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+  },
 
-   queryFromUrl : function(url) {
-      var vars = (url || document.URL).replace(/.+\?/, "").split("&"),
-          query = {};
-      for (var i = 0; i < vars.length; i++) {
-         var pair = vars[i].split("=");
-         query[pair[0]] = decodeURIComponent(pair[1]);
-      }
-      return query;
-   },
+  queryFromUrl: function(url) {
+    var vars = (url || document.URL).replace(/.+\?/, "").split("&"),
+        query = {};
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      query[pair[0]] = decodeURIComponent(pair[1]);
+    }
+    return query;
+  },
 
-   queryString : function(query) {
-      var string = [];
-      for (key in query) {
-         string.push(encodeURIComponent(key) + "=" + encodeURIComponent(query[key]));
-      }
-      return string.join("&");
-   },
+  queryString: function(query) {
+    var string = [];
+    for (key in query) {
+      string.push(encodeURIComponent(key) + "=" + encodeURIComponent(query[key]));
+    }
+    return string.join("&");
+  },
 
-   byTime : function(event1, event2) {
-      return new Date(event2.time) - new Date(event1.time);
-   },
+  byTime: function(event1, event2) {
+    return new Date(event2.time) - new Date(event1.time);
+  },
 
-   idify : function(name) {
-     return name.replace(/\W/g, "-");
-   },
+  idify: function(name) {
+    return name.replace(/\W/g, "-");
+  },
 
-   spinner : function(elem, inline) {
+  spinner: function(elem, inline) {
     var spinner = $("<img src='lib/indicator.gif' class='spinner'></img>");
     if (inline) {
-      spinner.css({display: 'inline'});
+      spinner.css({
+        display: 'inline'
+      });
     }
     elem.append(spinner);
   }
